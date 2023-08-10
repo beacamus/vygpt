4db5bb6a1835968894a54152be2c504b64b5b50d
beacamus <bea@heffalumps.org>
2023-08-10 16:25:04 +0100

    Add very specific square detector

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -37,11 +37,66 @@ public class GameController : MonoBehaviour {
         - Clear the list to signify the end of a shape
         - Reset the persistence**/
         if (Input.GetButtonDown("Fire2")) {
+            int lines = newShape.Count - 1;
+            Debug.Log(lines);
             for (var i = 0; i < newShape.Count; i++) {
                 if (i != 0) {
+                    //Debug.Log(newShape[i-1] + " " + newShape[i]);
                     Debug.DrawLine(newShape[i-1],newShape[i], Color.red, persistence, false);
                 }
             }
+            switch (lines) {
+                case 1:
+                    Debug.Log("Line");
+                    break;
+                case 2:
+                    Debug.Log("Angle");
+                    break;
+                case 3:
+                    Debug.Log("Triangle");
+                    break;
+                case 4:
+                    Vector3[] exampleSquare = { new Vector3 (2,0,0), new Vector3 (0,-2,0), new Vector3 (-2,0,0), new Vector3 (0,2,0)};
+                    //Vector3[] positions = { new Vector3 { x = 0, y = 0, z = 0 }, new Vector3 { x = 1, y = 1, z = 1} }; 
+                    Debug.Log("Square");
+                    Vector3 v_one= newShape[1] - newShape[0];
+                    Vector3 v_two= newShape[2] - newShape[1];
+                    Vector3 v_three= newShape[3] - newShape[2];
+                    Vector3 v_four= newShape[0] - newShape[3];
+                    Debug.Log(v_one + " " + v_two + " " + v_three + " " + v_four);
+                    Vector3 sim1 = v_one - exampleSquare[0];
+                    Vector3 sim2 = v_two - exampleSquare[1];
+                    Vector3 sim3 = v_three - exampleSquare[2];
+                    Vector3 sim4 = v_four - exampleSquare[3];
+                    Debug.Log(sim1 + " " + sim2 + " " + sim3 + " " + sim4);
+                    bool angle1 = false;
+                    bool angle2 = false;
+                    bool angle3 = false;
+                    bool angle4 = false;
+                    if ((sim1.x < 0.4) && (sim1.y < 0.4)) {
+                        angle1 = true;
+                    }
+                    if ((sim2.x < 0.4) && (sim2.y < 0.4)) {
+                        angle2 = true;
+                    }
+                    if ((sim3.x < 0.4) && (sim3.y < 0.4)) {
+                        angle3 = true;
+                    }
+                    if ((sim4.x < 0.4) && (sim4.y < 0.4)) {
+                        angle4 = true;
+                    }
+                    Debug.Log(sim1 + " " + angle1);
+                    Debug.Log(sim2 + " " + angle2);
+                    Debug.Log(sim3 + " " + angle3);
+                    Debug.Log(sim4 + " " + angle4);
+                    if (angle1 && angle2 && angle3 && angle4) {
+                        Debug.Log("AHHHH");
+                    }
+                    //xyxy < 0.2
+                    break;
+            }
+
+
             newShape.Clear();
             persistence = 0.0f;
         }
